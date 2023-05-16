07b8165a4950d50ae453ceacf075968cbbce3c59
YermakovOLV <133772236+YermakovOLV@users.noreply.github.com>
2023-05-16 17:02:04 +0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function changeColor() {
+    let h1 = document.getElementById('text');
+    h1.style.color = 'green';
+}
+
+function highlight() {
+    let h1 = document.getElementById('text');
+    h1.classList.add('highlight');
+}
